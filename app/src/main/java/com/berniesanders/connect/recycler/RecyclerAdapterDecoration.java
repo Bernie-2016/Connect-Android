b21ecd445c3a67da47c226afe6998b447869c405
Daniel Tashjian <thewopple@gmail.com>
2016-02-28 14:02:13 -0500

    Added comments to RecyclerAdapterDecoration

--- app/src/main/java/com/berniesanders/connect/recycler/RecyclerAdapterDecoration.java
+++ app/src/main/java/com/berniesanders/connect/recycler/RecyclerAdapterDecoration.java
@@ -5,15 +5,42 @@ import android.support.v7.widget.RecyclerView.ViewHolder;
 import android.view.ViewGroup;
 
 public interface RecyclerAdapterDecoration<VH extends ViewHolder> extends HasMaxViewType, DataSetChangeable {
+    /**
+     * @param position position in adapter + decoration
+     * @return true if the position belongs to the adapter, false if it belongs to the decoration
+     */
     boolean isAdapterPosition(final Adapter<VH> adapter, final int position);
 
+    /**
+     * @param position position in adapter + decoration
+     * @return position in adapter
+     */
     int getAdapterPosition(final Adapter<VH> adapter, final int position);
 
+    /**
+     * @param viewType view type in the decoration
+     * @return view holder for the decoration
+     * @see {@link Adapter#onCreateViewHolder(ViewGroup, int)}
+     */
     VH onCreateViewHolder(final ViewGroup parent, final int viewType);
 
+    /**
+     * Bind the view holder for the decoration.
+     * @param position position in adapter + decoration
+     * @see {@link Adapter#onBindViewHolder(VH, int)}
+     */
     void onBindViewHolder(final Adapter<VH> adapter, final VH holder, final int position);
 
+    /**
+     * @param position position in adapter + decoration
+     * @return view type in the decoration
+     * @see {@link Adapter#getItemViewType(int)}
+     */
     int getItemViewType(final Adapter<VH> adapter, final int position);
 
+    /**
+     * @return number of items in the decoration
+     * @see {@link Adapter#getItemCount()}
+     */
     int getItemCount(final Adapter<VH> adapter);
 }
